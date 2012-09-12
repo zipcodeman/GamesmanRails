293f54afdb01961000dc223c0fbc85434d6556e7
Zachary Bush <zmbush@berkeley.edu>
2012-09-12 11:56:29 -0700

    Add files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamesmanRails::Application.config.secret_token = 'f9966dc5598caf587c3a172a1bd8403efc2f43701648ef4a72bba477a03e1a7fa11d7802575d7ba4bec16df682abb6b3ce6cc35a1a81a7cad4cdb06c4ab28e47'
