cd09a74c024ae2d1b67cc45dbe75f2927c3cf116
Zachary Bush <zmbush@berkeley.edu>
2012-09-12 17:33:44 -0700

    Add asset images for main page

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,5 +1,13 @@
 class BaseController < ApplicationController
   def index
+    @games = {
+      "Tic-Tac-Toe" => "ttt",
+      "Connect 4" => "c4",
+      "Bagh-Chal" => "baghchal",
+      "Othello" => "othello",
+      "Atari Go" => "atarigo",
+      "Connections" => "connections",
+    }
     render :index
   end
 end
