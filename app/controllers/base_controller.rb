0f97e70b6409f59129f12c0e910f0da2648e7590
Zachary Bush <zach@zmbush.com>
2012-09-26 19:17:55 -0700

    add game directory

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,6 +1,18 @@
 class BaseController < ApplicationController
   layout "base_layout"
   def index
+    @games = {}
+    Dir.glob("app/assets/javascripts/games/*") do |file|
+      parts = file.split('/')
+      filename = parts[-1].split('.')
+      
+      asset_name = parts[-1].split('.')[0]
+      game_name = File.open(file).first.split[1]
+      @games[game_name] = asset_name
+    end
+
+    return
+
     @games = {
       "Tic-Tac-Toe" => "ttt",
       "Connect 4" => "c4",
